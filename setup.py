aa46fd41c8d2263dcfdd33ce4279afb69733f21b
Arwichok <arwichok@gmail.com>
2019-07-01 07:04:10 +0300

    change version to 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fastconf",
-    version="0.0.3",
+    version="0.0.4",
     author="Arwichok",
     keywords="config",
     license="MIT",
